98082ad11c61d8a20a1b0d50f2ac7de5deb73c0f
Feng Han <fenghan@Fengs-MacBook-Pro.local>
2020-09-25 11:29:40 -0700

    add hw_w7.py file

---
+++ hw_w7.py
@@ -0,0 +1,23 @@
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try:
+        value=int(num)
+        valid=1
+    except:
+        valid=0
+        #continue
+    if valid==1:
+        if smallest==None:
+            smallest=value
+            if largest==None:
+            smallest=value
+        elif value>largest:
+            largest=value
+    else:
+        print('Invalid input')
+print('Maximum is',largest)
+print('Minimum is',smallest)
